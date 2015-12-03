d901a7c4ca4c1a13ab185636f00da92d97b17193
Asuka <kimrapowawa@gmail.com>
2015-12-04 02:31:45 +0900

    repository

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  
+  #新規登録
+  def new
+    @user = User.new
+  end
+
+  #マイページを表示させる
+  def show
+  	@user = User.find(params[:id])
+  end
+
+
+  def create
+   @user = User.new(user_params) 
+    if @user.save
+      log_in @user
+   	 flash[:success] = "Welcome to REAL!"
+     redirect_to @user
+   else
+      render 'new'
+   end
+  end
+
+   private
+
+     def user_params
+       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+     end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+class User < ActiveRecord::Base
+  
+  #saveする前にアドレスを小文字に統一して一意性保つ
+  before_save { self.email = email.downcase }
+
+  #コンテンツとの関係
+  has_many :Spots
+
+  #写真アップロード
+  mount_uploader :picture, PictureUploader
+
+  #名前とアドレスのバリデーション
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  #パスワード設定
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+
+   # 与えられた文字列のハッシュ値を返す 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  
+end
